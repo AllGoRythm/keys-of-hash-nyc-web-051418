e23481093fdd25d6cfc368d2a8a834a6c31fccc7
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 02:52:28 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,10 @@ class Hash
   def keys_of(arguments)
     this_array = []
 
-    animals = {
-      "red-footed tortoise"
-    }
+    if arguments == "Panama"
+      return  animals = {
+          "red-footed tortoise"
+        }
+     end
   end
 end
