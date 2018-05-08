fcb93137829910abc4ef1afed0543c5b785cc3b8
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 03:05:57 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,13 +3,8 @@ class Hash
   def keys_of(arguments)
      collect = []
 
-       arguments.each do |animal, country|
-         binding.pry
-         if arguments == "Panama"
-           collect << animal
-         end
-       end
-       collect
-
+     arguments.each do |variable|
+       binding.pry
+     end
    end
 end
