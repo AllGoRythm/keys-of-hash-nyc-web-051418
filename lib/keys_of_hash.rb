e33afa4bab902d65a7ee63b6edf9530e0c38b85a
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 03:27:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -7,7 +7,7 @@ class Hash
     # from the hash whose value matches the value(s) given as an argument.
      country = []
 
-     arguments.each do |variable|
+     arguments.each do |country|
 
      end
 end
