42084d25dc9ae514bb619a163d7e7b89d6532f0f
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 03:09:21 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,11 @@
 require "pry"
 class Hash
   def keys_of(*arguments)
-     collect = []
+    # This method should take an an undefined number of arguments,
+    # using the splat operator,
+    # and return an array with every key
+    # from the hash whose value matches the value(s) given as an argument.
+     country = []
 
      arguments.each do |k, v|
        binding.pry
