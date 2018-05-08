c01546f6f050bed534126ee532e5550ce37fbe43
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 03:03:40 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,12 +1,15 @@
 require "pry"
 class Hash
   def keys_of(arguments)
-     []
+     collect = []
 
-     if arguments == "Panama"
        animals.each do |animal, country|
          binding.pry
+         if arguments == "Panama"
+           collect << animal
+         end
        end
-     end
+       collect
+
    end
 end
