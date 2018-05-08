f059b9c93abd576d0dfff33d838d4036682cd4fb
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 02:56:34 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,7 @@
 class Hash
   def keys_of(arguments)
-     animal: ["red-footed tortoise"]
+     :animal => ["red-footed tortoise"]
+
      if arguments == "Panama"
        return [:animal]
      end
